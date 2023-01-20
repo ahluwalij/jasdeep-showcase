514af029b367aebc98f97983ab13441440d94ff4
ahluwalij <jaspay6@gmail.com>
2023-01-20 10:20:40 -0500

    npm install next-themes @heroicons/react

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ export default function HomePage() {
       <main>
         <section className='bg-white'>
           <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-            <Vercel className='text-5xl' />
+            <Vercel className='bg-blue-500 text-5xl dark:bg-red-500' />
             <h1 className='mt-4'>
               Next.js + Tailwind CSS + TypeScript Starter
             </h1>
