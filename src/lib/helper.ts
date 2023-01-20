42fae4f2c17953feacb17b15bc9105dfb7c5f7f6
ahluwalij <jaspay6@gmail.com>
2023-01-19 23:43:06 -0500

    Initial Commit

---
+++ src/lib/helper.ts
@@ -0,0 +1,37 @@
+type OpenGraphType = {
+  siteName: string;
+  description: string;
+  templateTitle?: string;
+  logo?: string;
+};
+export function openGraph({
+  siteName,
+  templateTitle,
+  description,
+  logo = 'https://jasdeep-ahluwalia.s3.amazonaws.com/website-icon.png',
+}: OpenGraphType): string {
+  const ogLogo = encodeURIComponent(logo);
+  const ogSiteName = encodeURIComponent(siteName.trim());
+  const ogTemplateTitle = templateTitle
+    ? encodeURIComponent(templateTitle.trim())
+    : undefined;
+  const ogDesc = encodeURIComponent(description.trim());
+
+  return `https://og.jasdeepahluwalia.com/api/general?siteName=${ogSiteName}&description=${ogDesc}&logo=${ogLogo}${
+    ogTemplateTitle ? `&templateTitle=${ogTemplateTitle}` : ''
+  }`;
+}
+
+export function getFromLocalStorage(key: string): string | null {
+  if (typeof window !== 'undefined') {
+    return window.localStorage.getItem(key);
+  }
+  return null;
+}
+
+export function getFromSessionStorage(key: string): string | null {
+  if (typeof sessionStorage !== 'undefined') {
+    return sessionStorage.getItem(key);
+  }
+  return null;
+}
