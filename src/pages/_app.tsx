2c888cc3977be805a1078228fab20c2791a2a543
ahluwalij <jaspay6@gmail.com>
2023-05-01 17:44:22 -0400

    feat: analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
+import { Analytics } from '@vercel/analytics/react';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import React from 'react';
 
 import '@/styles/globals.css';
-
 /**
  * ? `Layout` component is called in every page using `np` snippets. If you have consistent layout across all page, you can add it here too
  */
@@ -17,6 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute='class'>
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
