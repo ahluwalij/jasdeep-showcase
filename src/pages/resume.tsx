0c56ebf3dfc7ae3b726cdbafd5b77604f1677067
ahluwalij <jaspay6@gmail.com>
2023-05-23 12:46:18 -0400

    feat: resume

---
+++ src/pages/resume.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+import PrimaryLink from '@/components/links/PrimaryLink';
+
+export default function Resume() {
+  return (
+    <div className='h-fit overflow-visible'>
+      <object
+        data='assets/Jasdeep_Ahluwalia_Resume.pdf'
+        type='application/pdf'
+        width='100%'
+        height='200%'
+        className='h-screen'
+      >
+        <div className='flex h-screen items-center justify-center text-center'>
+          <div>
+            It seems like my PDF resume isn't loading for you(most likely
+            mobile). However, feel free to go through my{' '}
+            <PrimaryLink href='/work'>Work Experience</PrimaryLink> and my{' '}
+            <PrimaryLink href='/projects'>Projects</PrimaryLink> so you can
+            still get to know me!
+          </div>
+        </div>
+      </object>
+    </div>
+  );
+}
