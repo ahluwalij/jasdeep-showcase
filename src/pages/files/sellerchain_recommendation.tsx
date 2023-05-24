557e32e8a98f131ee3cfbfb267b4742557c38702
ahluwalij <jaspay6@gmail.com>
2023-05-24 18:22:42 -0400

    feat: sellerchain recommendation

--- src/pages/files/sellerchain_recommendation.tsx
+++ src/pages/files/sellerchain_recommendation.tsx
@@ -6,16 +6,16 @@ export default function Resume() {
   return (
     <div className='h-fit overflow-visible'>
       <object
-        data='assets/Jasdeep_Ahluwalia_Resume.pdf'
+        data='../assets/Jasdeep_Ahluwalia_Sellerchain_Recommendation.pdf'
         type='application/pdf'
         width='100%'
-        height='200%'
+        height='100%'
         className='h-screen'
       >
         <div className='flex h-screen items-center justify-center text-center'>
           <div>
-            It seems like my PDF resume isn't loading for you(most likely
-            mobile). However, feel free to go through my{' '}
+            It seems like my PDF recommendation isn't loading for you(most
+            likely mobile). However, feel free to go through my{' '}
             <PrimaryLink href='/work'>Work Experience</PrimaryLink> and my{' '}
             <PrimaryLink href='/projects'>Projects</PrimaryLink> so you can
             still get to know me!
