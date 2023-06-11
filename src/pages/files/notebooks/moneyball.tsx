dcc3a77534da987a2b5c92946b7f43ffdf4c2f75
ahluwalij <jaspay6@gmail.com>
2023-06-11 02:44:18 -0400

    fix: moneyball update

--- src/pages/files/notebooks/moneyball.tsx
+++ src/pages/files/notebooks/moneyball.tsx
@@ -7,7 +7,7 @@ import '@react-pdf-viewer/core/lib/styles/index.css';
 import ButtonLink from '@/components/links/ButtonLink';
 import Seo from '@/components/Seo';
 
-<Viewer fileUrl='../../assets/Moneyball_Project.pdf' />;
+<Viewer fileUrl='../../assets/Moneyball_Baseball_Project.pdf' />;
 
 export default function Moneyball_Project() {
   return (
@@ -26,7 +26,7 @@ export default function Moneyball_Project() {
       </header>
       <div className='bg-white py-10'>
         <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js'>
-          <Viewer fileUrl='../../assets/Moneyball_Project.pdf' />
+          <Viewer fileUrl='../../assets/Moneyball_Baseball_Project.pdf' />
         </Worker>
       </div>
     </main>
