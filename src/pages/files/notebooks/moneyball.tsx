69b528cad385360cc4246cf0fd5030a3a2935844
ahluwalij <jaspay6@gmail.com>
2023-06-11 02:27:29 -0400

    feat: uploaded notebooks

---
+++ src/pages/files/notebooks/moneyball.tsx
@@ -0,0 +1,34 @@
+import { Worker } from '@react-pdf-viewer/core';
+import { Viewer } from '@react-pdf-viewer/core';
+import * as React from 'react';
+
+import '@react-pdf-viewer/core/lib/styles/index.css';
+
+import ButtonLink from '@/components/links/ButtonLink';
+import Seo from '@/components/Seo';
+
+<Viewer fileUrl='../../assets/Moneyball_Project.pdf' />;
+
+export default function Resume() {
+  return (
+    <main>
+      <Seo templateTitle='Resume' />
+      <header className='fixed top-0 z-10 w-full bg-transparent'>
+        <div className='flex h-14 items-center justify-end pr-4'>
+          <nav>
+            <ul className='flex items-center justify-between space-x-4'>
+              <ButtonLink variant='dark' href='/'>
+                Home
+              </ButtonLink>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <div className='bg-white py-10'>
+        <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js'>
+          <Viewer fileUrl='../../assets/Moneyball_Project.pdf' />
+        </Worker>
+      </div>
+    </main>
+  );
+}
