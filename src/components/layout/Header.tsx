5a7d720fc882aaed2405066cc138214f3b0504d3
ahluwalij <jaspay6@gmail.com>
2024-08-28 18:51:04 -0400

    fix: fixed header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,18 +1,21 @@
+import clsx from 'clsx';
+import { useRouter } from 'next/router';
 import * as React from 'react';
 import { useState } from 'react';
 
 import DarkModeButton from '@/components/buttons/DarkModeButton';
+import PrimaryLink from '@/components/links/PrimaryLink';
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
-
+  const router = useRouter();
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
   return (
     <header className='fixed top-0 z-10 w-full bg-transparent'>
-      <div className='flex h-14 items-center justify-between px-8'>
+      <div className='flex h-14 items-center justify-end px-8'>
         {/* Hamburger Icon */}
         <div className='md:hidden' onClick={toggleMenu}>
           <div className={`hamburger ${isOpen ? 'open' : ''}`}>
@@ -26,7 +29,51 @@ export default function Header() {
         <nav className={`${isOpen ? 'block' : 'hidden'} md:block`}>
           <ul className='flex flex-col items-center justify-between space-y-4 md:flex-row md:space-x-4 md:space-y-0'>
             {/* Navigation Links */}
-            {/* ... your PrimaryLink components ... */}
+            <PrimaryLink
+              className={clsx(
+                'hidden dark:text-[#FAD5A5]',
+                router.pathname != '/' ? 'block' : ''
+              )}
+              href='/'
+            >
+              Home
+            </PrimaryLink>
+            <PrimaryLink
+              className={clsx(
+                'hidden dark:text-[#FAD5A5]',
+                router.pathname != '/work' ? 'block' : ''
+              )}
+              href='/work'
+            >
+              Work
+            </PrimaryLink>
+            <PrimaryLink
+              className={clsx(
+                'hidden dark:text-[#FAD5A5]',
+                router.pathname != '/education' ? 'block' : ''
+              )}
+              href='/education'
+            >
+              Education
+            </PrimaryLink>
+            <PrimaryLink
+              className={clsx(
+                'hidden dark:text-[#FAD5A5]',
+                router.pathname != '/projects' ? 'block' : ''
+              )}
+              href='/projects'
+            >
+              Projects
+            </PrimaryLink>
+            <PrimaryLink
+              className={clsx(
+                'hidden dark:text-[#FAD5A5]',
+                router.pathname != '/aiml' ? 'block' : ''
+              )}
+              href='/aiml'
+            >
+              AI/ML
+            </PrimaryLink>
             <DarkModeButton />
           </ul>
         </nav>
