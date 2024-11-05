7978db6e0d3ffcce1d301b464c62241306e892cf
ahluwalij <jaspay6@gmail.com>
2024-11-04 21:43:59 -0500

    fix: test

--- src/pages/files/resume.tsx
+++ src/pages/files/resume.tsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import ButtonLink from '@/components/links/ButtonLink';
 import Seo from '@/components/Seo';
 
 export default function Resume() {
+  const [isFileFound, setIsFileFound] = useState(false);
+
+  useEffect(() => {
+    // Check if the resume file exists
+    fetch('../assets/Jasdeep_Ahluwalia_Resume.pdf')
+      .then((response) => {
+        if (response.ok) {
+          setIsFileFound(true);
+        } else {
+          setIsFileFound(false);
+        }
+      })
+      .catch(() => setIsFileFound(false));
+  }, []);
+
   return (
     <main className='flex h-screen flex-col'>
       <Seo templateTitle='Resume' />
       <div className='absolute hidden items-center justify-end bg-transparent pr-4 sm:right-[6.25rem] sm:top-[0.8rem] sm:flex md:right-28 md:top-[0.625rem]'>
-        <nav>
-          <ul className='flex items-center justify-between space-x-4'>
-            <ButtonLink variant='dark' href='/'>
-              Home
-            </ButtonLink>
-          </ul>
-        </nav>
+        {isFileFound && (
+          <nav>
+            <ul className='flex items-center justify-between space-x-4'>
+              <ButtonLink variant='dark' href='/'>
+                Home
+              </ButtonLink>
+            </ul>
+          </nav>
+        )}
       </div>
       <div className='flex-grow bg-white'>
         <iframe
